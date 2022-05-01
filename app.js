ff72b8c616b1dfa8fddeaed315e1400dd8623e5a
yasith <it19966922@my.sliit.lk>
2022-05-01 20:48:37 +0530

    create shooping list

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 import react from 'react';
 import Register from "./src/components/Register/Register";
 import Home from "./src/components/Pages/Home Page/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navigation from "./src/components/shared/Navigation/Navigation";
+import WishList from "./src/components/Pages/WishList/WishList";
+import Cart from "./src/components/Pages/Cart/Cart";
+import ShoppingItemsPage from "./src/components/Pages/ShoppingItems/ShoppingItemsPage";
+import Promotion from "./src/components/Pages/Promotion/Promotion";
+
 
 const App = () => {
     return (
     <div>
-      <Home />
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/wishlist">
+                        <WishList />
+                    </Route>
+                    <Route path="/cart">
+                        <Cart />
+                    </Route>
+                    <Route path="/shopping-items">
+                        <ShoppingItemsPage />
+                    </Route>
+                    <Route path="/promotions">
+                        <Promotion />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 };
